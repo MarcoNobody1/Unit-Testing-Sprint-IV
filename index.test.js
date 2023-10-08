b2267ecdeda772ae0c65331ed584db2049b64632
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-08 22:59:50 +0200

    se crea test para comprobar que occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { Room, Booking } = require("./index.js");
 
 describe("Is room occupied?", () => {
-  test("isOccupied es false cuando la habitacion esta ocupada en una fecha dada", () => {
+  test("isOccupied es true cuando la habitacion esta ocupada en una fecha dada", () => {
     const bookings = [
       { startDate: "2023-10-02", endDate: "2023-10-06" },
       { startDate: "2023-10-08", endDate: "2023-10-12" },
@@ -10,6 +10,16 @@ describe("Is room occupied?", () => {
     const room1 = new Room("Room1", bookings, 150, 10);
     const isOccupied = room1.isOccupied("2023-10-05");
     expect(isOccupied).toBe(true);
-  })
+  }),
+    test("occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings", () => {
+      const bookings = [
+        { startDate: "2023-10-01", endDate: "2023-10-07" },
+        { startDate: "2023-10-08", endDate: "2023-10-15" },
+      ];
 
+      const room2 = new Room("Room2", bookings, 150, 10);
+      const percentage = room2.occupancyPercentage("2023-10-01", "2023-10-15");
+
+      expect(percentage).toBe(100);
+    });
 });
