797caa0b537e9d8637079e88e1bacd582e49bcbf
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-09 14:25:07 +0200

    se modifican datos del test para hacerlo acorde al documento funcional del sprint

--- index.test.js
+++ index.test.js
@@ -1,25 +1,133 @@
 const { Room, Booking } = require("./index.js");
 
-describe("Is room occupied?", () => {
+describe("Tests para la clase de Room", () => {
   test("isOccupied es true cuando la habitacion esta ocupada en una fecha dada", () => {
-    const bookings = [
-      { startDate: "2023-10-02", endDate: "2023-10-06" },
-      { startDate: "2023-10-08", endDate: "2023-10-12" },
-    ];
+    const room = {
+      name: "Room1",
+      rate: 150,
+      discount: 10,
+    };
 
-    const room1 = new Room("Room1", bookings, 150, 10);
+    const booking1 = new Booking(
+      "booking 1",
+      "bok@bok.es",
+      "2023-10-01",
+      "2023-10-06",
+      10,
+      room
+    );
+
+    const booking2 = new Booking(
+      "booking 2",
+      "bok2@bok.es",
+      "2023-10-07",
+      "2023-10-15",
+      10,
+      room
+    );
+
+    const bookings = [booking1, booking2];
+
+    const room1 = new Room(room.name, bookings, room.rate, room.discount);
     const isOccupied = room1.isOccupied("2023-10-05");
     expect(isOccupied).toBe(true);
-  }),
-    test("occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings", () => {
-      const bookings = [
-        { startDate: "2023-10-01", endDate: "2023-10-07" },
-        { startDate: "2023-10-08", endDate: "2023-10-15" },
-      ];
+  });
+  test("isOccupied es false cuando la habitacion no esta ocupada en una fecha dada", () => {
+    const room = {
+      name: "Room1",
+      rate: 150,
+      discount: 10,
+    };
 
-      const room2 = new Room("Room2", bookings, 150, 10);
-      const percentage = room2.occupancyPercentage("2023-10-01", "2023-10-15");
+    const booking1 = new Booking(
+      "booking 1",
+      "bok@bok.es",
+      "2023-10-01",
+      "2023-10-06",
+      10,
+      room
+    );
 
-      expect(percentage).toBe(100);
-    });
+    const booking2 = new Booking(
+      "booking 2",
+      "bok2@bok.es",
+      "2023-10-07",
+      "2023-10-15",
+      10,
+      room
+    );
+
+    const bookings = [booking1, booking2];
+
+    const room1 = new Room(room.name, bookings, room.rate, room.discount);
+    const isOccupied = room1.isOccupied("2023-10-17");
+    expect(isOccupied).toBe(false);
+  });
+  test("isOccupied es booleano con cualquier dato", () => {
+    const room = {
+      name: "Room1",
+      rate: 150,
+      discount: 10,
+    };
+
+    const booking1 = new Booking(
+      "booking 1",
+      "bok@bok.es",
+      "2023-10-01",
+      "2023-10-06",
+      10,
+      room
+    );
+
+    const booking2 = new Booking(
+      "booking 2",
+      "bok2@bok.es",
+      "2023-10-07",
+      "2023-10-15",
+      10,
+      room
+    );
+
+    const bookings = [booking1, booking2];
+
+    const room1 = new Room(room.name, bookings, room.rate, room.discount);
+    const isOccupied = room1.isOccupied();
+    const isOccupied2 = room1.isOccupied(3);
+    const isOccupied3 = room1.isOccupied("hola buenas");
+    expect(typeof isOccupied).toBe("boolean");
+    expect(typeof isOccupied2).toBe("boolean");
+    expect(typeof isOccupied3).toBe("boolean");
+  });
+  test("occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings", () => {
+    const room = {
+        name: "Room1",
+        rate: 150,
+        discount: 10,
+      };
+  
+      const booking1 = new Booking(
+        "booking 1",
+        "bok@bok.es",
+        "2023-10-01",
+        "2023-10-06",
+        10,
+        room
+      );
+  
+      const booking2 = new Booking(
+        "booking 2",
+        "bok2@bok.es",
+        "2023-10-07",
+        "2023-10-15",
+        10,
+        room
+      );
+  
+      const bookings = [booking1, booking2];
+
+    const room1 = new Room("Room1", bookings, 150, 10);
+    const percentage = room1.occupancyPercentage("2023-10-01", "2023-10-15");
+
+    expect(percentage).toBe(100);
+  });
 });
